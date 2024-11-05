6924290a88a76656365497862c7a1daed753c73d
Juliette Hutton <juliettehutton2@gmail.com>
2024-11-05 16:21:43 -0500

    working app, we can fetch the todos from the api, and display them on the screen, as well as delete one when we click on it

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { App } from './components/App';
+import { reducers } from './reducers';
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
